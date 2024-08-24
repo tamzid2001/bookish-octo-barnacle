6f31e9a8e3089f2ebbbcc52515fdc622169d4b2c
System Administrator <root@Tamzids-MacBook-Pro.local>
2024-08-24 15:44:03 -0400

    clipcraftai commit

--- src/app/page.js
+++ src/app/page.js
@@ -62,15 +62,7 @@ const Hero = () => (
           See AI-generated video in action:
         </Typography>
         <Box className="aspect-w-16 aspect-h-9">
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/ysz5S6PUM-U"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/NXpdyAWLDas?si=aFG6Iea_xFox59ur" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </Box>
       </Box>
     </Container>
